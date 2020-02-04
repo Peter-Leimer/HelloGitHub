df7615f9beca0690505ba86278c4434cf3a66e92
Peter-Leimer <peterleimer6@gmail.com>
2020-02-04 08:42:55 -0600

    Syntax notes

---
+++ HelloGit.py
@@ -0,0 +1,56 @@
+d=69
+print("Hello World!")
+c=420
+print(d+c)
+
+myList = [1,2,3]
+#how to stride
+print(mylist[0:6:2])  #from index 0 to 6 skip by 2
+
+#strings=lists
+
+phrase = 'Catch the dog'
+phrase[2]#t
+phrase[4]#h
+phrase[-1:0-3]# strides work backwards -1 is right, 0 is l$
+
+
+
+print(mylist[1])
+
+def sumFunction(a,b):
+	return a+b
+print(sumFunction(2,20))
+#how to call a function 
+print(sumfunction(2,20))
+
+x=10
+y=-10
+
+if(x==10):
+	x=5
+elif(y==-10):
+	y=5
+else:
+	x=y-x
+
+#loops
+mylist= [1, 3, 8, 412, 43, 2, 20]
+
+#length of list
+len(mylist)
+
+#for loop index
+for i in range (len(mylist)):
+	print(mylist[i])
+
+
+#for each loop
+for val in mylist:
+	print(val)
+#dictionaries
+lookup = {}
+lookup ['kc']= 'chiefs'
+lookup['ne']= 'patriots'
+lookup['la']= 'chargers'
+lookup['suck']= 'raiders'
